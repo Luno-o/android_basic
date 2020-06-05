705171a4ecc9505b8b11f5bd4b1f78aa781ab005
Nick <omegaphenix@mail.ru>
2020-06-05 22:13:10 +0300

    ViewAndLayout done v1

--- 06_ViewAndLayout/app/src/main/java/com/example/a06_viewandlayout/MainActivity.kt
+++ 06_ViewAndLayout/app/src/main/java/com/example/a06_viewandlayout/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.a06_viewandlayout
 
 import android.annotation.TargetApi
+import android.content.Context
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.View
 import android.widget.*
@@ -22,14 +25,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            val imageView = findViewById<ImageView>(R.id.image_view)
-            Glide.with(this).load("https://lh3.googleusercontent.com/proxy/5i_P691R5xE9YTVnfzQSTP2WQPmY49JsaHY9SFEaAsvBTHLdmgAZedA4_E2Jbj6JG9mFCvGPgfM-RjcwuwAFITbvR1SvJhLtUF_Lp_lAzgA-08TEKdZw_NguNdNT4um_o_OXkvvgwkbxhLKun-s7XjeNnQ").into(imageView)
-        loginButton.setOnClickListener{
+        val imageView = findViewById<ImageView>(R.id.image_view)
+        loginButton.isEnabled = false
+
+        Glide.with(this)
+            .load("https://syrlasu.kz/wp-content/uploads/2019/07/kak-razgovarivat-s-devushkoj-chtoby-ona-vlyubilas1-min.jpg")
+            .into(imageView)
+
+        edit_login.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                loginButton.isEnabled =
+                    s?.let{ it.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked }
+                        ?: false
+
+            }
+        })
+        edit_password.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                loginButton.isEnabled =s?.let{it.isNotEmpty() && edit_login.text.isNotEmpty() && checkbox.isChecked }?:false
+            }
+
+        })
+        checkbox.setOnClickListener {
+            loginButton.isEnabled =
+                edit_password.text.isNotEmpty() && edit_login.text.isNotEmpty() && checkbox.isChecked
+        }
+        loginButton.setOnClickListener {
             val progressBar = ProgressBar(this).apply {
-               layoutParams = ConstraintLayout.LayoutParams(
-                   ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                   ConstraintLayout.LayoutParams.WRAP_CONTENT).apply {
-               }
+                layoutParams = ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    topToTop = ConstraintLayout.LayoutParams.PARENT_ID
+                    bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+                    leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
+                    rightToRight = ConstraintLayout.LayoutParams.PARENT_ID
+                }
             }
             container.addView(progressBar)
             loginButton.isEnabled = false
@@ -42,10 +85,9 @@ class MainActivity : AppCompatActivity() {
                 edit_login.isEnabled = true
                 edit_password.isEnabled = true
                 checkbox.isEnabled = true
-                Toast.makeText(this,"Registration success", Toast.LENGTH_SHORT).show()
-            },2000)
+                Toast.makeText(this, "Registration success", Toast.LENGTH_SHORT).show()
+            }, 2000)
         }
 
     }
 }
-//edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
