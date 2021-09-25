5b2b2ffd08f8f6084ba8bf68c9bfc29d1f385d4d
Павленко Николай <omegaphenix@mail.ru>
2021-09-26 00:48:33 +0300

    Merge branch 'master' of D:\AndroidStudioProjects\android_basic with conflicts.

--- Fragments_1/app/src/main/java/com/example/a06_viewandlayout/LoginFragment.kt
+++ Fragments_1/app/src/main/java/com/example/a06_viewandlayout/LoginFragment.kt
@@ -1,7 +1,5 @@
 package com.example.a06_viewandlayout
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.text.Editable
@@ -10,74 +8,90 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.activity_main.buttonANR
+import kotlinx.android.synthetic.main.activity_main.checkbox
+import kotlinx.android.synthetic.main.activity_main.edit_login
+import kotlinx.android.synthetic.main.activity_main.edit_password
+import kotlinx.android.synthetic.main.activity_main.image_view
+import kotlinx.android.synthetic.main.activity_main.loginButton
+import kotlinx.android.synthetic.main.activity_main.textViewLa
+
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    val tag1 = "Main Activity"
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val imageView = requireView().findViewById<ImageView>(R.id.image_view)
-        Glide.with(this).load("https://goo.su/1FQ4").into(imageView)
-        DebugLogger.d(tagFragment, "OnCreate was called")
+    private var formState: FormState = FormState(false, "")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+
+
+        super.onActivityCreated(savedInstanceState)
+        DebugLogger.d(tag1, "OnCreate was called")
         if (savedInstanceState != null) {
             savedInstanceState.getParcelable<FormState>(KEY_VALUE) ?: error("Unexpected state")
         }
 
-
-
-        loginButton.isEnabled = false
+        Glide.with(this).load("https://goo.su/1FQ4").into(image_view)
+        getfra.isEnabled = false
         val regexMail = Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]")
         val regexPass = Regex("[a-zA-Z0-9]")
-
-        buttonANR.setOnClickListener {
+        buttonANR.setOnClickListener()
+        {
             Thread.sleep(6000)
-            Toast.makeText(context,"ANR but press", Toast.LENGTH_SHORT).show()
-            DebugLogger.d(tagFragment,"ANR PRESS")
         }
-        edit_login.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
+        edit_login.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                loginButton.isEnabled =
-                    edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
-            }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    loginButton.isEnabled =
+                        edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
+                }
 
-        })
+            })
 
-        edit_password.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-            }
+        edit_password.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                loginButton.isEnabled =
-                    edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
-            }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    loginButton.isEnabled =
+                        edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
+                }
 
-        })
-
-        checkbox.setOnClickListener {
+            })
+        checkbox.setOnClickListener()
+        {
             loginButton.isEnabled =
                 edit_login.text.isNotEmpty() && edit_password.text.isNotEmpty() && checkbox.isChecked
         }
-
-        loginButton.setOnClickListener {
-            val progressBar = ProgressBar(context).apply {
+        loginButton.setOnClickListener()
+        {
+            val progressBar = ProgressBar(activity).apply {
                 layoutParams = ConstraintLayout.LayoutParams(
                     ConstraintLayout.LayoutParams.WRAP_CONTENT,
                     ConstraintLayout.LayoutParams.WRAP_CONTENT
@@ -88,14 +102,13 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                     leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID
                 }
             }
-            loginContainer.addView(progressBar)
-            DebugLogger.d(tagFragment,"login PRESS")
+            container.addView(progressBar)
             loginButton.isEnabled = false
             edit_login.isEnabled = loginButton.isEnabled
             edit_password.isEnabled = loginButton.isEnabled
             checkbox.isEnabled = loginButton.isEnabled
             Handler().postDelayed({
-              loginContainer.removeView(progressBar)
+                container.removeView(progressBar)
                 loginButton.isEnabled = true
                 edit_login.isEnabled = true
                 edit_password.isEnabled = true
@@ -106,22 +119,18 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                         val success = "Registration success"
                         textViewLa.text = success
                         formState.message = success
-                          Toast.makeText(context, success, Toast.LENGTH_SHORT).show()
-                            parentFragmentManager.beginTransaction()
-                                .replace(R.id.container,MainFragment())
-                                .commit()
-
+                        Toast.makeText(activity, success, Toast.LENGTH_SHORT).show()
                     } else {
                         val invalid = "Invalid password"
                         textViewLa.text = invalid
                         formState.message = invalid
-                             Toast.makeText(context, "Invalid password", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(activity, "Invalid password", Toast.LENGTH_SHORT).show()
                     }
                 } else {
                     val invalid = "Invalid Login"
                     textViewLa.text = invalid
                     formState.message = invalid
-                    Toast.makeText(context, "Invalid login", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity, "Invalid login", Toast.LENGTH_SHORT).show()
                 }
             }, 2000)
 
@@ -130,48 +139,36 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        DebugLogger.d(tagFragment,"onAttach")
-    }
-    companion object {
-
-        private const val KEY_VALUE = "key_value"
-        private const val KEY_TEXT = "key_text"
-
-        fun newInstance(text: String): LoginFragment {
-            return LoginFragment().withArguments { putString(KEY_TEXT, text) }
-        }
-    }
-
-    private val tagFragment = "Fragment Activity"
-    private var formState: FormState = FormState(false, "")
-
-
     override fun onPause() {
         super.onPause()
-        DebugLogger.i(tagFragment, "onPause was called")
+        DebugLogger.i(tag1, "onPause was called")
     }
 
     override fun onStart() {
         super.onStart()
-        DebugLogger.d(tagFragment, "OnStart was called")
+        DebugLogger.d(tag1, "OnStart was called")
     }
 
     override fun onStop() {
         super.onStop()
 
-        DebugLogger.w(tagFragment, "OnStop was called")
+        DebugLogger.w(tag1, "OnStop was called")
     }
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        formState =
+            savedInstanceState?.getParcelable<FormState>(KEY_VALUE) ?: error("Unexpected state")
+        textViewLa.text = formState.message
+    }
 
     override fun onResume() {
         super.onResume()
-        DebugLogger.v(tagFragment, "OnResume was called")
+        DebugLogger.v(tag1, "OnRes  ume was called")
     }
 
     override fun onDestroy() {
-        DebugLogger.e(tagFragment, "onDestroy was called")
+        DebugLogger.e(tag1, "onDestroy was called")
         super.onDestroy()
     }
 
@@ -208,8 +205,15 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
     }
 
+    companion object {
+        private const val KEY_VALUE = "formState"
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putParcelable(KEY_VALUE, formState)
+
     }
+
 }
+
--- Fragments_1/app/src/main/java/com/example/a06_viewandlayout/MainActivity.kt
+++ Fragments_1/app/src/main/java/com/example/a06_viewandlayout/MainActivity.kt
@@ -15,44 +15,14 @@ import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.activity_main.edit_login
 import kotlinx.android.synthetic.main.activity_main.view.*
 import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tabletSize = resources.getBoolean(R.bool.isTablet)
-
-
-        if (savedInstanceState==null) showLoginFragment()
-
-    }
-
-    private fun showLoginFragment(){
-
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container,LoginFragment())
-                .commit()              //ассинхронное добавление
-
-
-    }
-
-    override fun onBackPressed() {
-        for (fragment in supportFragmentManager.fragments){
-            if (fragment.isVisible){
-                val childFragment = fragment.childFragmentManager
-                if (childFragment.backStackEntryCount > 0){
-                    childFragment.popBackStack()
-                Log.d("Main backstack", childFragment.backStackEntryCount.toString())
-                }
-                else
-                    super.onBackPressed()
-            }
-        }
 
     }
 }
