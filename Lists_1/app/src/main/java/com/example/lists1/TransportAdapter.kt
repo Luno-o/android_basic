cd401b84f9148e4e5995a105e3d00bc61bd36eb4
Nick <omegaphenix@mail.ru>
2022-06-23 13:14:04 +0300

    Homework lists1 add ripple and findcash

--- Lists_1/app/src/main/java/com/example/lists1/TransportAdapter.kt
+++ Lists_1/app/src/main/java/com/example/lists1/TransportAdapter.kt
@@ -10,10 +10,17 @@ import androidx.core.content.PackageManagerCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_aircraft.*
+import kotlinx.android.synthetic.main.item_aircraft.serialNumberTextView
+import kotlinx.android.synthetic.main.item_aircraft.transportModelTextView
+import kotlinx.android.synthetic.main.item_roadtransport.*
+import kotlinx.android.synthetic.main.item_watercraft.*
 import java.util.jar.Manifest
+import kotlinx.android.synthetic.main.item_aircraft.transportImageView as transportImageView1
 
-class TransportAdapter(private val onItemClick: (position: Int) -> Unit)
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class TransportAdapter(private val onItemClick: (position: Int) -> Unit) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var transports: List<Transport> = emptyList()
 
     override fun getItemViewType(position: Int): Int {
@@ -28,8 +35,14 @@ class TransportAdapter(private val onItemClick: (position: Int) -> Unit)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             TYPE_AIRCRAFT -> AircraftHolder(parent.inflate(R.layout.item_aircraft), onItemClick)
-            TYPE_WATERCRAFT -> WatercraftHolder(parent.inflate(R.layout.item_watercraft), onItemClick)
-            TYPE_ROADTRANSPORT -> RoadTransportHolder(parent.inflate(R.layout.item_roadtransport), onItemClick)
+            TYPE_WATERCRAFT -> WatercraftHolder(
+                parent.inflate(R.layout.item_watercraft),
+                onItemClick
+            )
+            TYPE_ROADTRANSPORT -> RoadTransportHolder(
+                parent.inflate(R.layout.item_roadtransport),
+                onItemClick
+            )
             else -> error("Incorrect viewType $viewType")
         }
     }
@@ -38,17 +51,17 @@ class TransportAdapter(private val onItemClick: (position: Int) -> Unit)
         when (holder) {
             is AircraftHolder -> {
                 val transport = transports[position].let { it as? Transport.AirCraft }
-                        ?: error("Transport at position = $position is not Aircraft")
+                    ?: error("Transport at position = $position is not Aircraft")
                 holder.bind(transport)
             }
             is WatercraftHolder -> {
                 val transport = transports[position].let { it as? Transport.WaterCraft }
-                        ?: error("Transport at position = $position is not Watercraft")
+                    ?: error("Transport at position = $position is not Watercraft")
                 holder.bind(transport)
             }
             is RoadTransportHolder -> {
                 val transport = transports[position].let { it as? Transport.RoadTransport }
-                        ?: error("Transport at position = $position is not RoadTransport")
+                    ?: error("Transport at position = $position is not RoadTransport")
                 holder.bind(transport)
             }
             else -> {
@@ -61,69 +74,73 @@ class TransportAdapter(private val onItemClick: (position: Int) -> Unit)
     fun updateTransports(newTransports: List<Transport>) {
         transports = newTransports
     }
-    abstract class BaseTransportHolder(
-        val view: View, onItemClick: (position: Int) -> Unit
-    ) : RecyclerView.ViewHolder(view) {
 
-        private val TAG = "ImageLink"
-        private val transportImageView: ImageView = view.findViewById(R.id.transportImageView)
-        private val transportModelTextView: TextView = view.findViewById(R.id.transportModelTextView)
-        private val serialNumberTextView: TextView = view.findViewById(R.id.serialNumberTextView)
+
+    abstract class BaseTransportHolder(
+        final override val containerView: View, onItemClick: (position: Int) -> Unit
+    ) : RecyclerView.ViewHolder(containerView),LayoutContainer {
+
+
         init {
-            view.setOnClickListener {
+            containerView.setOnClickListener {
                 onItemClick(adapterPosition)
             }
         }
 
-        protected fun bindMainInfo(model: String,
-        imageLink: String) {
-            transportModelTextView.text = view.resources.getString(R.string.model,model)
-            serialNumberTextView.text = view.resources.getString(R.string.serial_number,getRandomString((4..7).random()))
+        protected fun bindMainInfo(
+            model: String,
+            imageLink: String
+        ) {
+            transportModelTextView.text = containerView.resources.getString(R.string.model, model)
+            serialNumberTextView.text =
+                containerView.resources.getString(R.string.serial_number, getRandomString((4..7).random()))
 
-            Log.d(TAG, "Link: $imageLink")
             Glide.with(itemView)
                 .load(imageLink)
                 .placeholder(R.drawable.ic_transport)
-                .into(transportImageView)
+                .into(transportImageView1)
 
         }
 
         private fun getRandomString(length: Int): String {
             val allowChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
             return (1..length)
-                    .map { allowChars.random() }
-                    .joinToString("")
+                .map { allowChars.random() }
+                .joinToString("")
         }
     }
 
     class AircraftHolder(
-            view: View, onItemClick: (position: Int) -> Unit)
-        : BaseTransportHolder(view, onItemClick) {
-        private val flightRangeTextView = view.findViewById<TextView>(R.id.flightRangeTextView)
+        view: View, onItemClick: (position: Int) -> Unit
+    ) : BaseTransportHolder(view, onItemClick) {
+
         fun bind(transport: Transport.AirCraft) {
-            bindMainInfo(transport.model,transport.imageLink)
-            flightRangeTextView.text = view.resources.getString(R.string.flight_range,transport.flightRange)
+            bindMainInfo(transport.model, transport.imageLink)
+            flightRangeTextView.text =
+                containerView.resources.getString(R.string.flight_range, transport.flightRange)
         }
     }
 
-    class WatercraftHolder(view: View, onItemClick: (position: Int) -> Unit)
-        : BaseTransportHolder(view, onItemClick) {
-        private val displacementTextView: TextView = view.findViewById(R.id.displacementTextView)
+    class WatercraftHolder(view: View, onItemClick: (position: Int) -> Unit) :
+        BaseTransportHolder(view, onItemClick) {
+
         fun bind(transport: Transport.WaterCraft) {
-            bindMainInfo(transport.model,transport.imageLink)
-            displacementTextView.text = view.resources.getString(R.string.displacement,transport.displacement)
+            bindMainInfo(transport.model, transport.imageLink)
+            displacementTextView.text =
+                containerView.resources.getString(R.string.displacement, transport.displacement)
         }
     }
 
     class RoadTransportHolder(
-            view: View, onItemClick: (position: Int) -> Unit
+        view: View, onItemClick: (position: Int) -> Unit
     ) : BaseTransportHolder(view, onItemClick) {
-        private val fuelConsumptionTextView: TextView = view.findViewById(R.id.fuelConsumptionTextView)
-        fun bind(transport: Transport.RoadTransport) {
-            bindMainInfo(transport.model,transport.imageLink)
-            fuelConsumptionTextView.text = view.resources.getString(R.string.fuelConsumption,transport.fuelConsumption)}
-        }
 
+        fun bind(transport: Transport.RoadTransport) {
+            bindMainInfo(transport.model, transport.imageLink)
+            fuelConsumptionTextView.text =
+                containerView.resources.getString(R.string.fuelConsumption, transport.fuelConsumption)
+        }
+    }
 
 
     companion object {
