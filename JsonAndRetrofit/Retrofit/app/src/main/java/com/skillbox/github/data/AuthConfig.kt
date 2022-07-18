befe3ee78f0dea65d23d91e294e6fa02c9ce9d72
Nick <omegaphenix@mail.ru>
2022-07-18 19:37:17 +0300

    Retrofit init

--- JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
+++ JsonAndRetrofit/Retrofit/app/src/main/java/com/skillbox/github/data/AuthConfig.kt
@@ -9,7 +9,7 @@ object AuthConfig {
     const val RESPONSE_TYPE = ResponseTypeValues.CODE
     const val SCOPE = "user,repo"
 
-    const val CLIENT_ID = "YOUR CLIENT ID"
-    const val CLIENT_SECRET = "YOUR CLIENT SECRET"
+    const val CLIENT_ID = "3fec507e7f3278a3d336"
+    const val CLIENT_SECRET = "b9e7da4cb54fea7218bf7e372a8823a5bf715ae7"
     const val CALLBACK_URL = "skillbox://skillbox.ru/callback"
 }
